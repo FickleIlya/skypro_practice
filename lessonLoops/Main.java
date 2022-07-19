33f4a0ad83f872e14884615f203880f539ca2bcd
FickleIlya <fickle041@gmail.com>
2022-07-19 18:42:48 +0300

    lessonLoops

---
+++ lessonLoops/Main.java
@@ -0,0 +1,42 @@
+package lessonLoops;
+
+public class Main {
+    public static void main(String[] args) {
+        task3();
+    }
+
+    public static void task1(){
+        int num = 0;
+
+        while (num < 10){
+            num += 1;
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+
+        for (; num >= 1; num--){
+            System.out.print(num + " ");
+        }
+
+    }
+
+    public static void task2(int firstFriday){
+
+        int friday = firstFriday;
+        while (friday < 31){
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
+            friday += 7;
+        }
+    }
+
+    public static void task3(){
+        int period = 79;
+
+        for (int year = 0; year < 2100; year += period){
+            if (year >= 1800){
+                System.out.println(year);
+            }
+        }
+    }
+}
